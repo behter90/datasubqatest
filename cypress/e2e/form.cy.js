c122eca729599f96d09ff5a9b5f1c87877be87d6
behter90 <behter1990@gmail.com>
2025-04-29 08:13:30 +0300

    Update form.cy.js

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,24 +1,28 @@
 import SubmitForm from "../pages/SubmitForm";
+import {slowCypressDown} from "cypress-slow-down";
 describe('Тест формы Request A Quote', () => {
     beforeEach(()=> {
-        //cy.visit('https://qatest.datasub.com/')
         SubmitForm.visit()
     })
-    it ('Успешная отправка формы с корректными данными', () => {
-        //cy.get('#name').type('tester')
+    it ('Успешная отправка формы с корректными данными формы', () => {
+        slowCypressDown(1000)
         SubmitForm.fillName('tester')
-        //cy.get('#email').type('test@test.com')
         SubmitForm.fillEmail('test@test.com')
-        //cy.get('select').select(1).invoke('val').should('eq', 'B Service')
-        SubmitForm.selectService('B Service')
-        //cy.get('#message').type('test message')
+        cy.get('select').select(1).invoke('val').should('eq', 'B Service')
         SubmitForm.fillMessage('test message')
-        //cy.contains ('Request A Quote').click()
         SubmitForm.submit()
-        //cy.get('#formStatus').should('contain', 'Форма отправлена.')
         SubmitForm.checkSuccessMessage('Форма отправлена.')
 
     })
+    it ('Неуспешная отправка формы с корректными данными формы', () => {
+        slowCypressDown(1000)
+        SubmitForm.fillName('tester')
+        SubmitForm.fillEmail('test@test.com')
+        cy.get('select').select(0).invoke('val').should('eq', 'A Service')
+        SubmitForm.fillMessage('test message')
+        SubmitForm.submit()
+        SubmitForm.checkSuccessMessage('Форма отправлена.')
 
+    })
 
 })
